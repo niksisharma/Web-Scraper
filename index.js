c6e2512b2f33fa948aad1ac8dfca29a4a7692b8d
Nikita Sharma <51536421+niksisharma@users.noreply.github.com>
2021-09-27 10:06:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const PORT = 8000
 const express = require('express')
-const cheerio = require('cheerio');
-const axios = require('axios');
+const cheerio = require('cheerio')
+const axios = require('axios')
 
 const app = express()
 
