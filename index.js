c4ff8838c282f99b91d8f970b88d7c18945deaef
Nikita Sharma <51536421+niksisharma@users.noreply.github.com>
2021-09-27 10:04:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const PORT = 8000
+const express = require('express')
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+const app = express()
+
+const url = 'https://www.theguardian.com/uk'
+
+axios(url)
+	.then(response => {
+		const html =response.data
+		const $ = cheerio.load(html)
+		const atricles = []
+
+		$('.fc-item-title', html).each(function(){
+			const title = $(this).text()
+			const url = $(this).find('a').attr('href')
+			atricles.push({
+				title,
+				url
+			})
+		})
+		console.log(atricles)
+	}).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log('server running on ${PORT}'))
